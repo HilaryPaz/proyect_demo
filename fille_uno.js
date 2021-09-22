8d55056f5e451dd8bcb38298022a77f3913dfe52
Hilary Paz <hila.p.30.18@gmail.com>
2021-09-22 09:45:05 -0500

    Nuevo cambio

---
+++ fille_uno.js
@@ -0,0 +1,22 @@
+var num_uno =5;
+var num_dos =7;
+document.write("<h1>")
+document.write(num_uno^num_dos);//OR exllusivo: 0010
+document.write("</h1>")
+document.write("<br>")
+
+
+var dato4=2.7;
+
+var dato1=document.write((3.5/4.7)+2+(2.7/(3.7/3.6)));
+var dato2=document.write(((2.7+(5.9/2.9))/4.8)+3.8);
+var dato3=document.write(4.3+2.3);//debe dar 1.68
+document.write("<br>")
+document.write("</h1>")
+document.write(dato1/dato2);
+document.write("</h1>")
+document.write("<br>")
+document.write(dato3)
+document.write("</h1>")
+document.write(num_uno^num_dos);
+document.write("</h1>")
